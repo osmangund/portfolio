d7ff85d44d6b2136610cac0cd32a79b0f44565b6
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-02-06 19:26:00 +0300

    feat: add  projectContents bodies

--- src/components/sections/Projects/projectContents.js
+++ src/components/sections/Projects/projectContents.js
@@ -1,27 +1,27 @@
 export const projectContents = [
   {
     title: "News Page",
-    body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde corporis tempora, debitis nesciunt rem nemo! Nulla, blanditiis distinctio? Temporibus blanditiis, possimus inventore saepe alias, odio praesentium voluptas vel corporis, voluptatem numquam corrupti. Odio impedit delectus sint optio rem, distinctio accusantium aspernatur eius similique esse facere nisi! Amet maiores saepe doloribus?",
+    body: "Practised UI/UX design. Articles on this news page app are reusable components. Used styled-components to style. Works without an error on mobile devices.",
     tags: ["React", "styled-components"],
   },
   {
     title: "Pomodoro Clock",
-    body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde corporis tempora, debitis nesciunt rem nemo! Nulla, blanditiis distinctio? Temporibus blanditiis, possimus inventore saepe alias, odio praesentium voluptas vel corporis, voluptatem numquam corrupti. Odio impedit delectus sint optio rem, distinctio accusantium aspernatur eius similique esse facere nisi! Amet maiores saepe doloribus?",
+    body: "While creating this pomodoro timer project, I used React. By using it, it was easier to work with the state management, timer logic and dividing the app to eliminate the confusion. Also, there are two themes to choose from: space and nature. More, the app offers four different work - break timer presets, and it rings when the timer ends. Responsively designed.",
     tags: ["HTML", "CSS", "React"],
   },
   {
     title: "Data Visualization Projects",
-    body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde corporis tempora, debitis nesciunt rem nemo! Nulla, blanditiis distinctio? Temporibus blanditiis, possimus inventore saepe alias, odio praesentium voluptas vel corporis, voluptatem numquam corrupti. Odio impedit delectus sint optio rem, distinctio accusantium aspernatur eius similique esse facere nisi! Amet maiores saepe doloribus?",
+    body: "Worked with five different APIs and used D3.js to display them. Created a bar chart, scatter plot, heat map, choropleth map, and a treemap. With the use of React, merged all the projects into one app. On treemap, handled three different datasets on single page.",
     tags: ["HTML", "CSS", "React", "D3", "APIs"],
   },
   {
     title: "Fitness Website",
-    body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde corporis tempora, debitis nesciunt rem nemo! Nulla, blanditiis distinctio? Temporibus blanditiis, possimus inventore saepe alias, odio praesentium voluptas vel corporis, voluptatem numquam corrupti. Odio impedit delectus sint optio rem, distinctio accusantium aspernatur eius similique esse facere nisi! Amet maiores saepe doloribus?",
+    body: "With using React, created reusable components that can further be used to improve this 111 Fitness Page or to create new fitness websites. Used styled-components to style the components and framer-motion to add animations. Works without an error on mobile devices.",
     tags: ["React", "styled-components", "framer-motion"],
   },
   {
     title: "Random Quote Generator",
-    body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde corporis tempora, debitis nesciunt rem nemo! Nulla, blanditiis distinctio? Temporibus blanditiis, possimus inventore saepe alias, odio praesentium voluptas vel corporis, voluptatem numquam corrupti. Odio impedit delectus sint optio rem, distinctio accusantium aspernatur eius similique esse facere nisi! Amet maiores saepe doloribus?",
+    body: "Worked with Random Quote Generator API to fetch quotes and display them on the page. Used React to create the app, and react-spinners to create a loading spinner. App is fully responsive and changes color with each new quote.",
     tags: ["HTML", "CSS", "APIs", "React"],
     color: "#36191d",
   },
