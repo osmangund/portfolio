080959cd6b93356c17cdb62ac40331e3e4c17e2b
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-02-08 13:03:10 +0300

    feat: finish form success/fail responses with react-hot-toast

--- src/components/sections/Contact/Contact.jsx
+++ src/components/sections/Contact/Contact.jsx
@@ -1,29 +1,71 @@
 import { GithubLogo } from "../../icons/Github"
 import { LinkedinLogo } from "../../icons/Linkedin"
 import "./Contact.scss"
-import { useEffect, useRef, useState } from "react"
+import { useRef } from "react"
 import emailjs from "@emailjs/browser"
 import PropTypes from "prop-types"
+import toast, { Toaster } from "react-hot-toast"
 
-const InputField = ({ name, label, type = "text" }) => {
+const InputField = ({ element = "input", name, label, type = "text" }) => {
   return (
     <div className="input">
-      <input
-        type={type}
-        name={name}
-        id={name}
-        required
-        placeholder=""
-        className="input-field"
-      />
+      {element === "input" ? (
+        <input
+          type={type}
+          name={name}
+          id={name}
+          required
+          placeholder=""
+          className="input-field"
+        />
+      ) : (
+        <textarea
+          name={name}
+          id={name}
+          required
+          placeholder=""
+          className="input-field"
+        />
+      )}
       <label htmlFor={name}>{label}</label>
     </div>
   )
 }
 
+const sendSuccessMessage = () => {
+  return toast.success(
+    "You have sent your message successfully! I'll reply back as soon as possible.",
+    {
+      duration: 3000,
+      style: {
+        border: "1px solid var(--accent-color)",
+        padding: "16px",
+        fontSize: "1.5rem",
+      },
+      iconTheme: {
+        primary: "var(--accent-color)",
+        secondary: "#FFFAEE",
+      },
+    }
+  )
+}
+
+const sendErrorMessage = () => {
+  return toast.error("Failed to send your message. Please try again later.", {
+    duration: 3000,
+    style: {
+      padding: "16px",
+      fontSize: "1.5rem",
+    },
+    iconTheme: {
+      primary: "red",
+      secondary: "#FFFAEE",
+    },
+  })
+}
+
 const ContactForm = () => {
   const form = useRef()
-
   const sendEmail = (e) => {
     e.preventDefault()
 
@@ -34,11 +76,10 @@ const ContactForm = () => {
       .then(
         () => {
           e.target.reset()
-          setFormSent("success")
+          sendSuccessMessage()
         },
-        (error) => {
-          console.log("FAILED...", error.text)
-          setFormSent("fail")
+        () => {
+          sendErrorMessage()
         }
       )
   }
@@ -48,7 +89,11 @@ const ContactForm = () => {
       <form ref={form} onSubmit={sendEmail}>
         <InputField name="user_name" label="Full name" />
         <InputField name="user_email" label="Email" />
-        <InputField name="message" label="Hi Osman, I have an idea..." />
+        <InputField
+          element="textarea"
+          name="message"
+          label="Hi Osman, I have an idea..."
+        />
         <button>Submit</button>
       </form>
     </>
@@ -56,37 +101,6 @@ const ContactForm = () => {
 }
 
 export default function Contact() {
-  const [formsent, setFormSent] = useState(null)
-  const FormSuccess = () => {
-    useEffect(() => {
-      setTimeout(() => {
-        setFormSent(null)
-      }, 3000)
-    })
-    return (
-      <div className="form__info__card">
-        <p>Thanks for reaching out! I&apos;ll get back to you soon.</p>
-      </div>
-    )
-  }
-
-  const FormFail = () => {
-    useEffect(() => {
-      setTimeout(() => {
-        setFormSent(null)
-      }, 3000)
-    })
-    return (
-      <div className="form__info__card">
-        <p>Oops! Something went wrong. Please try again later.</p>
-      </div>
-    )
-  }
-
-  const InfoCard = () => {
-    if (formsent === "success") return <FormSuccess />
-    if (formsent === "fail") return <FormFail />
-  }
   return (
     <section id="contact">
       <div id="contact__info">
@@ -108,16 +122,14 @@ export default function Contact() {
           </a>
         </div>
         <ContactForm />
-
         <p id="brand">
-          <b>osmangund</b>® 2024. All rights reserved.
+          <span className="highlight">osmangund</span>® 2024. All rights reserved.
         </p>
       </div>
       <div id="contact__image">
         <img src="imgs/contact.jpg" alt="" />
       </div>
-      <InfoCard />
-      <FormSuccess />
+      <Toaster position="top-right" />
     </section>
   )
 }
@@ -126,4 +138,5 @@ InputField.propTypes = {
   name: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   type: PropTypes.string,
+  element: PropTypes.string,
 }
