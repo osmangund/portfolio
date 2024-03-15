9e8f6fbb631af4353c40097e704a7517a98edca1
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-03-15 13:54:02 +0300

    feat: add to head fontfamily space grotesk

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,15 +2,22 @@ import { Html, Head, Main, NextScript } from "next/document"
 import { manrope, poppins, space_grotesk } from "@/utils/fonts"
 export default function Document() {
   return (
-    <Html
-      lang="en"
-      className={`${space_grotesk.variable} ${poppins.variable} ${manrope.variable}`}
-    >
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${space_grotesk.style.fontFamily};
+        }
+      `}</style>
+      <Html
+        lang="en"
+        className={`${space_grotesk.variable} ${poppins.variable} ${manrope.variable}`}
+      >
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    </>
   )
 }
